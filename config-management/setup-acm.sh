6ed0f7aa2b05975b8ebf2dc9fb66a42580a13d3b
[Rakesh Parkiti] <[rakeshparkiti@gmail.com]>
2020-06-12 11:24:59 +0530

    Adding Repo initial commit

---
+++ config-management/setup-acm.sh
@@ -0,0 +1,21 @@
+#!/bin/bash
+
+# This quick script onboards clusters to Anthos Config Management
+
+export KUBECONFIG=$HOME/.kube/destiny-kubeconfig 
+kubectl apply -f - <<EOF
+# config-management.yaml
+
+apiVersion: configmanagement.gke.io/v1
+kind: ConfigManagement
+metadata:
+  name: config-management
+spec:
+  # clusterName is required and must be unique among all managed clusters
+  clusterName: destiny
+  git:
+    syncRepo: git@github.com:rakeshparkiti/OverallOrg.git
+    syncBranch: master
+    secretType: ssh
+    policyDir: "BU1"
+EOF
